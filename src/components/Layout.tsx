79971e4cc3994c8a0499cf7818c71bb8d126d2d8
AdamKniec <adam.k.kniec@gmail.com>
2021-05-20 22:35:32 +0200

    scss compination feature and some initial navbar styling

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,11 +6,10 @@
  */
 
 import React from "react";
-import PropTypes from "prop-types";
 // import { useStaticQuery, graphql } from "gatsby";
 
 import Header from "./Navbar";
-import "./layout.css";
+import "../styles/css/combined.css";
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   // const data = useStaticQuery(graphql`
@@ -47,9 +46,4 @@ const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     </>
   );
 };
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-};
-
 export default Layout;
