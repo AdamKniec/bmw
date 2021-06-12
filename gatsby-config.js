e76a40c59318c137883cc19edbbbb08702b3647a
AdamKniec <adam.k.kniec@gmail.com>
2021-06-12 23:14:58 +0200

    clean-up in gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,13 +6,6 @@ module.exports = {
   },
   plugins: [
     {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images/`,
-      },
-    },
-    {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
         fonts: [`Roboto`, `Roboto:300`, `Roboto:400`, `Roboto:700`],
