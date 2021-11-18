354d48c914b92c18cf1bf6efdf26dd3c2609b06e
Adam Knieć <adam.kniec@netguru.com>
2021-11-18 21:41:08 +0100

    adding the styles for the more info section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -128,6 +128,40 @@ const Header = (props: HeaderProps) => {
           </div>
         </div>
       </section>
+      <section className="info-section">
+        <div className="info-section-inner-wrapper">
+          <h2 className="section-header">
+            więcej <span className="theme-red">info</span>
+          </h2>
+          <div className="issue-example">
+            <p className="question">Znalazłeś buga lub błąd merytoryczny?</p>
+            <p className="answer">
+              Zgłoś issue na{" "}
+              <a className="external-link" href="linkgithub">
+                githubie
+              </a>{" "}
+            </p>
+          </div>
+          <div className="issue-example">
+            <p className="question">Chcesz się skontaktować w innej sprawie?</p>
+            <p className="answer">
+              Napisz na{" "}
+              <a className="external-link" href="mailto:adam.k.kniec@gmail.com">
+                adam.k.kniec@gmail.com
+              </a>
+            </p>
+          </div>
+          <div className="issue-example">
+            <p className="question">Umiesz w instagramy?</p>
+            <p className="answer">
+              Zapraszam na mojego{" "}
+              <a className="external-link" href="instagrama">
+                instagrama!
+              </a>
+            </p>
+          </div>
+        </div>
+      </section>
     </Layout>
   );
 };
